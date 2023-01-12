2104a6a3b32eda9ecd36a294b3d3331c36786e4f
mgarrote <martindambrowskigarrote@gmail.com>
2023-01-12 06:21:29 -0300

    Criação de rota para buscar por nome

--- server.py
+++ server.py
@@ -24,4 +24,13 @@ def list_products():
     result.headers.add("Access-Control-Allow-Origin", "*")
     return result
 
+# by name
+@app.route("/products/search/<str:product_name>")
+def searchby_name(product_name):
+    products = Product.query.filter(Product.name.contains(product_name))
+    if len(products) == 0:
+        result = new_result("not found", "there are no products that meet the specified specifications on database")
+    else:
+        result = new_result("success", to_json(products))
+
 app.run(debug = True)
