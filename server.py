6a4ed0273a64fdaf14c4b916574b2f9dfcbf1a9b
mgarrote <martindambrowskigarrote@gmail.com>
2023-01-12 06:41:02 -0300

    Terminada rota de buscar pelo nome, que estava incompleta.

--- server.py
+++ server.py
@@ -25,12 +25,15 @@ def list_products():
     return result
 
 # by name
-@app.route("/products/search/<str:product_name>")
+@app.route("/products/search/<string:product_name>")
 def searchby_name(product_name):
-    products = Product.query.filter(Product.name.contains(product_name))
+    products = Product.query.filter(Product.name.contains(product_name)).all()
+    print(products)
     if len(products) == 0:
         result = new_result("not found", "there are no products that meet the specified specifications on database")
     else:
         result = new_result("success", to_json(products))
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
 
 app.run(debug = True)
